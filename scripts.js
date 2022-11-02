edf47859a22a47a82a3f18eb9e34fc8f233bdb14
Takacs-Balazs <71711153+Takacs-Balazs@users.noreply.github.com>
2022-11-02 19:37:43 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function orderBlur(x) {
+    x.style.background = "#fff";
+    x.style.background = "#fff";
+    x.style.background = "#fff";
+}
+function elso_mezo() {
+    var name = document.getElementById("nev").value;
+    var address = document.getElementById("cim").value;
+    var tel = document.getElementById("telefonszam").value;
+    document.getElementById("nev_visszaig").innerHTML = "Név: "+ name;
+    document.getElementById("cim_visszaig").innerHTML = "Cím: "+ address; 
+    document.getElementById("telefon_visszaig").innerHTML = "Telefon: "+ tel;
+}
+function orderFocus(y) {
+    y.style.background = "#99ccff";
+    y.style.background = "#99ccff";
+    y.style.background = "#99ccff";
+}
